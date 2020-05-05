e3b5da987d1e8adefd95021315a99eb4ae0eebfb
OptSolution <mr_cwang@foxmail.com>
2020-05-05 20:47:43 +0800

    init

---
+++ readme.md
@@ -0,0 +1,17 @@
+Tikz-tool
+==========
+
+Depends on 
+- [tikzjax](http://tikzjax.com) 
+- [electron](https://www.electronjs.org)
+
+## Usage
+For the first time:
+```
+npm install
+```
+
+Then draw in the `index.html` file and run by
+```
+npm start
+```
---
+++ src/main.js
@@ -0,0 +1,70 @@
+/*
+ * @Description: 
+ * @Author: Chen Wang
+ * @Email: mr_cwang@foxmail.com
+ * @Date: 2020-05-05 19:49:38
+ * @LastEditors: Chen Wang
+ * @LastEditTime: 2020-05-05 20:39:37
+ */
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, Menu } = require('electron')
+
+function createWindow() {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    // and load the index.html of the app.
+    mainWindow.loadFile('index.html')
+
+    // Add mune
+    const mainMenuTemplate = [
+        {
+            label: "Tikz-tool",
+            submenu: [
+                {
+                    label: "Quit",
+                    accelerator: "CmdOrCtrl+Q",
+                    role: "quit"
+                }
+            ]
+        // },
+        // {
+        //     label: "Help",
+        //     submenu: [
+        //         {
+        //             label: 'Devtool',
+        //             accelerator: 'CmdOrCtrl+D',
+        //             click() {
+        //                 mainWindow.webContents.openDevTools();
+        //             }
+        //         }
+        //     ]
+        }
+    ];
+    const menu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(menu);
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
