4340359f3b827a20b2aa583e0cfd12563d7ed92c
Chen Wang <mr_cwang@foxmail.com>
2020-05-06 19:24:57 +0800

    read any file

--- src/renderer.js
+++ src/renderer.js
@@ -4,7 +4,7 @@
  * @Email: mr_cwang@foxmail.com
  * @Date: 2020-05-05 21:53:06
  * @LastEditors: Chen Wang
- * @LastEditTime: 2020-05-06 18:56:01
+ * @LastEditTime: 2020-05-06 19:24:38
  */
 const { ipcRenderer, remote } = require('electron');
 const fs = require('fs');
@@ -32,7 +32,6 @@ window.addEventListener('contextmenu', (e) => {
 
 var output = document.getElementById('draw');
 var texFile = null;
-// update();
 
 //监听与主进程的通信
 ipcRenderer.on('action', (event, arg) => {
@@ -65,29 +64,16 @@ function openFile() {
   });
 }
 
-async function addTexDom() {
-  const s = document.createElement('script');
-  s.setAttribute('type', 'text/tikz');
+function update() {
+  let texDOM = document.createElement('script');
+  texDOM.setAttribute('type', 'text/tikz');
   fs.readFile(texFile, (err, data) => {
     if (err) throw err;
-    let text = document.createTextNode(data.toString());
-    s.appendChild(text);
-    // s.textContent = data.toString();
-    alert(text.nodeValue);
-    alert(s.childNodes[0].nodeValue);
+    texDOM.innerHTML = data.toString();
+    output.innerHTML = '';
+    output.appendChild(texDOM);
+    process_tikz(texDOM);
   })
-  output.innerHTML = '';
-  output.appendChild(s);
-}
-
-async function update() {
-  await addTexDom();
-  var scripts = document.getElementsByTagName('script');
-  var tikzScripts = Array.prototype.slice.call(scripts).filter(e => e.getAttribute('type') === 'text/tikz');
-  tikzScripts.reduce(async (promise, element) => {
-    await promise;
-    return process_tikz(element);
-  }, Promise.resolve());
 }
 
 function savePNG() {
